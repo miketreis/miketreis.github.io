820cb366c8c2b6ffab57cb5c1f857ff49f207029
mtreis1 <45612061+mtreis1@users.noreply.github.com>
2024-08-24 19:01:19 +0200

    v3

---
+++ assets/js/bg.js
@@ -0,0 +1,27 @@
+$(document).ready(function(){
+    $("#pic1").hover(function(){
+        $('#background_holder').css("background", "linear-gradient(to bottom, rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 1)), url('images/bg/1.jpg')");
+        $('#background_holder').css("background-size", "cover");
+        $('#background_holder').css("background-position", "center center");
+    })
+    $("#pic2").hover(function(){
+        $('#background_holder').css("background", "linear-gradient(to bottom, rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 1)), url('images/bg/2.jpg')");
+        $('#background_holder').css("background-size", "cover");
+        $('#background_holder').css("background-position", "center center");
+    })
+    $("#pic3").hover(function(){
+        $('#background_holder').css("background", "linear-gradient(to bottom, rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 1)), url('images/bg/3.jpg')");
+        $('#background_holder').css("background-size", "cover");
+        $('#background_holder').css("background-position", "center center");
+    })
+    $("#pic4").hover(function(){
+        $('#background_holder').css("background", "linear-gradient(to bottom, rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 1)), url('images/bg/4.jpg')");
+        $('#background_holder').css("background-size", "cover");
+        $('#background_holder').css("background-position", "center center");
+    })
+    $("#pic5").hover(function(){
+        $('#background_holder').css("background", "linear-gradient(to bottom, rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 1)), url('images/bg/5.jpg')");
+        $('#background_holder').css("background-size", "cover");
+        $('#background_holder').css("background-position", "center center");
+    })
+});
